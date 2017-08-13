c224b37c6793281543770893ab79b22731a7dd71
Mark Delos Reyes <makizworks@gmail.com>
2017-08-13 08:47:22 -0400

    fixed npm run dev server & client

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-//SET CREDENTIALS if in PRODUCTION or DEVELOPMENT
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod')
-} else {
-    module.exports = require('./dev')
+module.exports = {
+    googleClientID: '441393720218-rd0qt0be9ckcqmkc8m2flmgp8mv3o42f.apps.googleusercontent.com',
+    googleClientSecret: 'l967yVFhdmJVs9qEeQ7z4v_G',
+    mongoURI: 'mongodb://survie_admin:password0000@ds139761.mlab.com:39761/surviedb',
+    cookieKey: 'bdbodosdaoih092010912081-02-1kanaknxaxnxanxanxlnxan'
 }
